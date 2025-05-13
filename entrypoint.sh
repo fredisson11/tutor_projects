3f58b1e724661b5853c8e7c65d836e339fc81106
fredisson11 <185803622+fredisson11@users.noreply.github.com>
2025-05-13 19:49:50 +0200

    Update entrypoint.sh
    Signed-off-by: fredisson11 <185803622+fredisson11@users.noreply.github.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,10 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
+
+# 🔧 Replace __SPACE__ with spaces
+export EMAIL_HOST_PASSWORD=$(python3 -c "import os; print(os.environ['EMAIL_HOST_PASSWORD'].replace('__SPACE__', ' '))")
+echo "[DEBUG] EMAIL_HOST_PASSWORD after replace: >>>$EMAIL_HOST_PASSWORD<<<"
 
 echo "Applying database migrations..."
 python /app/manage.py migrate --noinput
